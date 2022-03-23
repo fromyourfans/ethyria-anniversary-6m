7856be38eee98441daab58eac35d1970b9d0b978
dragonjet <jetriconew@gmail.com>
2022-03-23 02:30:08 -0700

    Vue config no publicPath

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
-  publicPath: '/ethyria-anniversary-6m',
   outputDir: path.resolve(__dirname, 'docs'),
   transpileDependencies: [
     'vuetify',
