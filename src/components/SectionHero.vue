01aaeebf2fcf69b335e90bbff68b6d3e9fbac581
dragonjet <jetriconew@gmail.com>
2022-04-05 01:16:48 -0700

    Updated featured video

--- src/components/SectionHero.vue
+++ src/components/SectionHero.vue
@@ -1,14 +1,5 @@
 <template>
   <v-container class="section-grid section-box pb-8">
-    <!-- <v-row no-gutters class="mb-4">
-      <v-col cols="12">
-        <h1 class="yellow--text text--darken-3 text-center pa-0 mx-0 my-0">
-          <img
-            src="../assets/img/section-happy-congrats.png" height="90"
-            alt="Happy 6-Months and Congrats on 200K Millie!" />
-        </h1>
-      </v-col>
-    </v-row> -->
     <v-row no-gutters class="pt-8">
       <v-col cols="6"
         class="text-right white--text text-h5 text--darken-1 pt-4 pr-8 pl-4 hero-text">
@@ -23,7 +14,7 @@
       <v-col cols="6" class="pr-8">
         <iframe title="feature-video"
           width="100%" height="330"
-          src="https://www.youtube.com/embed/gx7AJmpHykc"
+          src="https://www.youtube.com/embed/ygS22EC6u2c"
           frameborder="0"
           allow="autoplay; clipboard-write; encrypted-media; picture-in-picture"
           allowfullscreen>
