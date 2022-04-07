184d8460eca1e395139338e545305f10b3f5940a
dragonjet <jetriconew@gmail.com>
2022-04-07 01:29:19 -0700

    Project credits

--- src/components/SectionCredits.vue
+++ src/components/SectionCredits.vue
@@ -2,7 +2,38 @@
   <v-container>
     <v-row no-gutters>
       <v-col cols="12" class="text-center black--text">
-        For Enna Alouette. From Aloupeeps.
+        <p>
+          For Enna Alouette. From Aloupeeps.
+        </p>
+        <p>
+          Management: Ennacord staff<br />
+          Programming: jetrico<br />
+          Header &amp; Background: Hel<br />
+          Aloupeeps on Cards: Frank<br />
+        </p>
+        <p>
+          <strong>Country Roads Acapella</strong><br />
+          Video: Soul Jam<br />
+          Arrangement: Soul Jam<br />
+          Alternate Lyrics: Soul Jam, guGGy<br />
+          Mix: Soul Jam<br />
+          Vocals: Acerola Heart, Binns, Crono, DarkDisasterKid, Dippy (Professional Simp),<br />
+            Domo Espresso, Drumfeelies, hiya! kk, IcyMeerkat, Jo-さん, koshka-sova, Norenche,<br />
+            Professor Nobu, Ralphy, Sasu Crow, Senno, Soul Jam, WhiteGuyGraal, zuiKatsu<br />
+          Artwork: MinniJay, Professor Nobu, Soul Jam<br />
+          A production of the Enna Alouette Fan Server<br />
+          Project Manager: Domo Espresso<br />
+          Server Owner: MiikeMQ
+        </p>
+        <p>
+          <strong>Art Quilt</strong><br />
+          Base Design: Garfield Factory<br />
+          Artists (in order, top-bottom, left-right):<br />
+            reteach, Professor Nobu, Jo-さん, Erin, drum,<br />
+            wister, wax apple, Sorekasho, oja, Toast,<br />
+            mirrorline, Garfield Factory, Hel, Frank, Hexvati,<br />
+            jetrico, Voldox, MinniJay, no1, sojuice
+        </p>
       </v-col>
     </v-row>
   </v-container>
