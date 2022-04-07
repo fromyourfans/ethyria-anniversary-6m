8dfc75aa4bdc30ee43244602176ff5e052ba017c
dragonjet <jetriconew@gmail.com>
2022-04-07 00:28:58 -0700

    Updated timeline clips and comments

--- src/components/SectionTimeline.vue
+++ src/components/SectionTimeline.vue
@@ -17,7 +17,7 @@
         <div class="rewind-left text-right">
           <iframe title="feature-video" class="rewind-frame"
             width="640" height="360"
-            src="https://www.youtube.com/embed/PKzGGJMCEmA"
+            src="https://www.youtube.com/embed/grKqMWfH22c"
             frameborder="0"
             allow="autoplay; clipboard-write; encrypted-media; picture-in-picture"
             allowfullscreen>
@@ -28,9 +28,18 @@
         </div>
         <div class="rewind-right black--text">
           <div class="rewind-texts">
-            <div class="card">
-              <div class="card-name text-h6 pr-12 py-2">{{cards[0].name}}</div>
-              <div class="card-text text-body-1 pr-4 pb-2">{{cards[0].message}}</div>
+            <div class="card mb-2">
+              <div class="card-text text-body-1 pa-2">
+                Perfect exhibit of Ethyria's early dynamics,
+                with a focus on Enna being a menace to her genmates (Nina especially).
+                In this heavenly house we do a little trolling, so it seems.
+              </div>
+            </div>
+            <div class="card mb-2">
+              <div class="card-text text-body-1 pa-2">
+                The way Nina loses her shit with how kusogaki Enna is,
+                it's something that never happened to this extent before and beyond this moment.
+              </div>
             </div>
           </div>
         </div>
@@ -40,9 +49,17 @@
       <v-col>
         <div class="rewind-left black--text text-right">
           <div class="rewind-texts">
-            <div class="card">
-              <div class="card-name text-h6 pr-12 py-2">{{cards[1].name}}</div>
-              <div class="card-text text-body-1 pr-4 pb-2">{{cards[1].message}}</div>
+            <div class="card mb-2">
+              <div class="card-text text-body-1 pa-2">
+                Enna loves Ooga Boogas as much as us,
+                and i never saw a person actually using Ooga Boogas before her. Fascinating.
+              </div>
+            </div>
+            <div class="card mb-2">
+              <div class="card-text text-body-1 pa-2">
+                Highlights from the RE7 streams, with the now-legendary ooga boogas.
+                The phrase even made it to the membership emotes (thanks roomie!).
+              </div>
             </div>
           </div>
         </div>
@@ -52,7 +69,7 @@
         <div class="rewind-right text-left">
           <iframe title="feature-video" class="rewind-frame"
             width="640" height="360"
-            src="https://www.youtube.com/embed/PKzGGJMCEmA"
+            src="https://www.youtube.com/embed/yAZCKfEKM5I"
             frameborder="0"
             allow="autoplay; clipboard-write; encrypted-media; picture-in-picture"
             allowfullscreen>
@@ -65,7 +82,7 @@
         <div class="rewind-left text-right">
           <iframe title="feature-video" class="rewind-frame"
             width="640" height="360"
-            src="https://www.youtube.com/embed/PKzGGJMCEmA"
+            src="https://www.youtube.com/embed/Eq6j0qaaYNA"
             frameborder="0"
             allow="autoplay; clipboard-write; encrypted-media; picture-in-picture"
             allowfullscreen>
@@ -76,9 +93,19 @@
         </div>
         <div class="rewind-right black--text">
           <div class="rewind-texts">
-            <div class="card">
-              <div class="card-name text-h6 pr-12 py-2">{{cards[2].name}}</div>
-              <div class="card-text text-body-1 pr-4 pb-2">{{cards[2].message}}</div>
+            <div class="card mb-2">
+              <div class="card-text text-body-1 pa-2">
+                We can't NOT include life advice from Enna.
+                Some of her best moments come from these lovely zatsudan streams,
+                with which I am positive many other listeners agree.
+              </div>
+            </div>
+            <div class="card mb-2">
+              <div class="card-text text-body-1 pa-2">
+                It's a really pertinent topic that
+                i'm sure is being lived by a lot of people right now.
+                Enna's zatsus are so wholesome.
+              </div>
             </div>
           </div>
         </div>
@@ -88,9 +115,19 @@
       <v-col>
         <div class="rewind-left black--text text-right">
           <div class="rewind-texts">
-            <div class="card">
-              <div class="card-name text-h6 pr-12 py-2">{{cards[3].name}}</div>
-              <div class="card-text text-body-1 pr-4 pb-2">{{cards[3].message}}</div>
+            <div class="card mb-2">
+              <div class="card-text text-body-1 pa-2">
+                This was a incredible moment for the Songbird,
+                where even "non-anime fan" music nerds said they were impressed with the quality.
+                (Yes. They had to listen to this.)
+              </div>
+            </div>
+            <div class="card mb-2">
+              <div class="card-text text-body-1 pa-2">
+                Enna's been doing a capella since her first week,
+                and man she delivers Every Time! She loves harmonizing,
+                and we love hearing her harmonize. It is only fair to highlight her singing.
+              </div>
             </div>
           </div>
         </div>
@@ -100,7 +137,7 @@
         <div class="rewind-right text-left">
           <iframe title="feature-video" class="rewind-frame"
             width="640" height="360"
-            src="https://www.youtube.com/embed/PKzGGJMCEmA"
+            src="https://www.youtube.com/embed/2txZlmtpYDM"
             frameborder="0"
             allow="autoplay; clipboard-write; encrypted-media; picture-in-picture"
             allowfullscreen>
@@ -113,7 +150,7 @@
         <div class="rewind-left text-right">
           <iframe title="feature-video" class="rewind-frame"
             width="640" height="360"
-            src="https://www.youtube.com/embed/PKzGGJMCEmA"
+            src="https://www.youtube.com/embed/UceTcVw-XzM"
             frameborder="0"
             allow="autoplay; clipboard-write; encrypted-media; picture-in-picture"
             allowfullscreen>
@@ -124,9 +161,18 @@
         </div>
         <div class="rewind-right black--text">
           <div class="rewind-texts">
-            <div class="card">
-              <div class="card-name text-h6 pr-12 py-2">{{cards[4].name}}</div>
-              <div class="card-text text-body-1 pr-4 pb-2">{{cards[4].message}}</div>
+            <div class="card mb-2">
+              <div class="card-text text-body-1 pa-2">
+                Ah, yes. "Never listen to chat" *wink wonk*.
+                Screams, curses, and most importantly, Betrayal.
+              </div>
+            </div>
+            <div class="card mb-2">
+              <div class="card-text text-body-1 pa-2">
+                It was legit one of the most desperate
+                and genuine screams of despair that i ever heard.
+                In this day, chat wasn't playing around.ヾ( •́д•̀ ;)ﾉ
+              </div>
             </div>
           </div>
         </div>
@@ -136,9 +182,18 @@
       <v-col>
         <div class="rewind-left black--text text-right">
           <div class="rewind-texts">
-            <div class="card">
-              <div class="card-name text-h6 pr-12 py-2">{{cards[5].name}}</div>
-              <div class="card-text text-body-1 pr-4 pb-2">{{cards[5].message}}</div>
+            <div class="card mb-2">
+              <div class="card-text text-body-1 pa-2">
+                "HELLO EVERYBODY , I AM THE GREAT ROSEMI LOVELOCK-SAMA!
+                WOOO~~~" HAHAHAHA THE IMPRESSION IS SO FUNNY! I CAN'T-
+              </div>
+            </div>
+            <div class="card mb-2">
+              <div class="card-text text-body-1 pa-2">
+                The offline collab! Enna does a good Rosemi impression,
+                if we only count her going "oh!" and focusing her eyes.
+                For real though, the collab was a blast!
+              </div>
             </div>
           </div>
         </div>
@@ -148,11 +203,47 @@
         <div class="rewind-right text-left">
           <iframe title="feature-video" class="rewind-frame"
             width="640" height="360"
-            src="https://www.youtube.com/embed/PKzGGJMCEmA"
+            src="https://www.youtube.com/embed/aNDMAkA5mn8"
             frameborder="0"
             allow="autoplay; clipboard-write; encrypted-media; picture-in-picture"
             allowfullscreen>
           </iframe>
+        </div>
+      </v-col>
+    </v-row>
+    <v-row no-gutters class="rewind-item">
+      <v-col>
+        <div class="rewind-left text-right">
+          <iframe title="feature-video" class="rewind-frame"
+            width="640" height="360"
+            src="https://www.youtube.com/embed/aL9ktaEDmEg"
+            frameborder="0"
+            allow="autoplay; clipboard-write; encrypted-media; picture-in-picture"
+            allowfullscreen>
+          </iframe>
+        </div>
+        <div class="rewind-center">
+          <div class="rewind-dot"></div>
+        </div>
+        <div class="rewind-right black--text">
+          <div class="rewind-texts">
+            <div class="card mb-2">
+              <div class="card-text text-body-1 pa-2">
+                This is TOO GOOD not to be mentioned.
+                The day aloupeeps became clowns. Jebaited left and right.
+                Why are we still here? Just to suffer?
+              </div>
+            </div>
+            <div class="card mb-2">
+              <div class="card-text text-body-1 pa-2">
+                "Oh no, it's so late, i can't believe
+                i'll miss the first english karaoke stream (｡•́︿•̀｡)"
+                Then i wake up in the next day...
+                and all the aloupeeps were still processing what just happened.
+                We are still proud Alouclowns.
+              </div>
+            </div>
+          </div>
         </div>
       </v-col>
     </v-row>
@@ -237,7 +328,6 @@ export default {
 
 .card {
   position:relative;
-  min-height:100px;
   width:100%;
   background:rgba(255, 255, 255, 0.3);
   padding:5px 20px;
@@ -247,9 +337,7 @@ export default {
     font-weight:bold;
   }
   .card-text {
-    white-space: pre-line;
     color:#000;
-    max-height:280px;
     overflow-y:auto;
     overflow-x:hidden;
   }
